9b3e0c1718dca67702182f71c282adc73d38900b
testproject-testing-cf <56145975+zyrill707@users.noreply.github.com>
2023-10-03 19:39:53 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -23,3 +23,55 @@ function convertToSketch() {
 }
 
 document.getElementById("convertButton").addEventListener("click", convertToSketch);
+
+
+const canvas = document.getElementById('sketchCanvas');
+const downloadButton = document.getElementById('downloadButton');
+const ctx = canvas.getContext('2d');
+
+
+
+function downloadCanvas() {
+    const image = canvas.toDataURL('image/png'); 
+    const link = document.createElement('a');
+    link.href = image;
+    link.download = 'canvas_image.png'; 
+    link.click();
+}
+
+
+downloadButton.addEventListener('click', downloadCanvas);
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const imageInput = document.getElementById("photoInput");
+    const convertButton = document.getElementById("converthd");
+    const outputCanvas = document.getElementById("outputCanvas");
+    const ctx = outputCanvas.getContext("2d");
+
+    convertButton.addEventListener("click", function() {
+        const file = imageInput.files[0];
+
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = function(event) {
+                const img = new Image();
+                img.onload = function() {
+
+                    const newWidth = 1920; 
+                    const newHeight = 1080; 
+
+                    outputCanvas.width = newWidth;
+                    outputCanvas.height = newHeight;
+
+
+                    ctx.drawImage(img, 0, 0, newWidth, newHeight);
+
+                    outputCanvas.style.display = "block";
+                };
+                img.src = event.target.result;
+            };
+            reader.readAsDataURL(file);
+        }
+    });
+});
